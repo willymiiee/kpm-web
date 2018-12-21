e82f45a421e8eb236bd995ad7fb661e570525346
Naufal Farras <mnaufalfarras.mail@gmail.com>
2018-12-21 10:45:21 +0700

    optimize footer code

--- resources/views/frontend/modules/footer.blade.php
+++ resources/views/frontend/modules/footer.blade.php
@@ -1,14 +1,14 @@
-<footer>
-    <div class="container">
-        <div class="copyright">
-            <span>&copy; 2019 PT. Kembar Putra Makmur. <br> All Rights Reserved.</span>
+<footer class="footer mt-4">
+    <div class="container d-flex justify-content-center justify-content-md-between align-items-center">
+        <div class="footer__copyright text-center text-md-left">
+            <span>&copy; 2019 PT. Kembar Putra Makmur.<br>All Rights Reserved.</span>
         </div>
-        <div class="footer-nav">
-            <a href="{{ url('/') }}">Home</a>
-            <a href="{{ url('/about') }}">About</a>
-            <a href="{{ url('/product') }}">Products</a>
-            <a href="{{ url('/infrastructure') }}">Infrastructure</a>
-            <a href="{{ url('/supplier') }}">Supplier</a>
+        <div class="footer__nav">
+            <a class="footer__nav-link mr-4" href="{{ url('/') }}">Home</a>
+            <a class="footer__nav-link mx-4" href="{{ url('/about') }}">About</a>
+            <a class="footer__nav-link mx-4" href="{{ url('/product') }}">Products</a>
+            <a class="footer__nav-link mx-4" href="{{ url('/infrastructure') }}">Infrastructure</a>
+            <a class="footer__nav-link ml-4" href="{{ url('/supplier') }}">Supplier</a>
         </div>
     </div>
 </footer>
