4457ae2f6b84ff5e53bae731cc9b793fa10f54c3
Naufal Farras <mnaufalfarras.mail@gmail.com>
2018-12-21 07:54:28 +0700

    add webpack new config since updated laravel-mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,11 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css')
+    .options({
+        autoprefixer: {
+            options: {
+                browsers: ['last 2 versions']
+            }
+        }
+    });
