0c77bcd56a76d02797470b15411beab0ccae78d2
Naufal Farras <mnaufalfarras.mail@gmail.com>
2019-02-18 16:12:12 +0700

    organized footer code

--- resources/views/frontend/modules/footer.blade.php
+++ resources/views/frontend/modules/footer.blade.php
@@ -1,28 +1,26 @@
 <footer class="footer">
     <div class="container d-flex justify-content-center justify-content-lg-between align-items-center">
-        <div class="footer__copyright text-center text-lg-left">
-            <div class="footer__copyright-contact text-center text-lg-left mb-5">
-                <p>Hubungi Kantor Pusat</p>
-                <div class="media">
-                    <svg class="mr-3 align-self-center" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                        stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call">
-                        <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                    </svg>
-                    <div class="media-body">
-                        <span class="d-inline-block">(0361) 9006481 atau</span>
-                        <span class="d-inline-block">(0361) 9006483</span>
-                    </div>
+        <div class="text-center text-lg-left">
+            <p class="footer-title mb-3 mb-lg-2">Hubungi Kantor Pusat</p>
+            <div class="media mb-5">
+                <svg class="mr-3 align-self-center" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+                    stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone-call">
+                    <path d="M15.05 5A5 5 0 0 1 19 8.95M15.05 1A9 9 0 0 1 23 8.94m-1 7.98v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
+                </svg>
+                <div class="media-body">
+                    <span class="d-inline-block">(0361) 9006481 atau</span>
+                    <span class="d-inline-block">(0361) 9006483</span>
                 </div>
             </div>
-            <span>&copy; 2019 PT. Kembar Putra Makmur.<br>All Rights Reserved.</span>
+            <span class="footer-copyright d-inline-block">&copy; 2019 PT. Kembar Putra Makmur.<br>All Rights Reserved.</span>
         </div>
-        <div class="footer__nav d-none d-lg-block">
-            <a class="footer__nav-link mr-3" href="{{ url('/') }}">Beranda</a>
-            <a class="footer__nav-link mx-3" href="{{ url('/tentang-kami') }}">Tentang Kami</a>
-            <a class="footer__nav-link mx-3" href="{{ url('/produk') }}">Produk</a>
-            <a class="footer__nav-link mx-3" href="{{ url('/infrastruktur') }}">Infrastruktur</a>
-            <a class="footer__nav-link ml-3" href="{{ url('/principal') }}">Principal</a>
-            <a class="footer__nav-link mx-3" href="{{ url('/kontak') }}">Hubungi Kami</a>
+        <div class="d-none d-lg-block">
+            <a class="footer-link mr-3" href="{{ url('/') }}">Beranda</a>
+            <a class="footer-link mx-3" href="{{ url('/tentang-kami') }}">Tentang Kami</a>
+            <a class="footer-link mx-3" href="{{ url('/produk') }}">Produk</a>
+            <a class="footer-link mx-3" href="{{ url('/infrastruktur') }}">Infrastruktur</a>
+            <a class="footer-link ml-3" href="{{ url('/principal') }}">Principal</a>
+            <a class="footer-link mx-3" href="{{ url('/kontak') }}">Hubungi Kami</a>
         </div>
     </div>
 </footer>
