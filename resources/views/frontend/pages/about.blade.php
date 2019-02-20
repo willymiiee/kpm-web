895396e800386aad142e613b2c7200e498d076cb
Naufal Farras <mnaufalfarras.mail@gmail.com>
2019-02-20 10:00:47 +0700

    optimized code  and adjust font-size in about page

--- resources/views/frontend/pages/about.blade.php
+++ resources/views/frontend/pages/about.blade.php
@@ -12,7 +12,6 @@ Tentang Kami
         </div>
     </header>
     <div class="content content-1 container row mx-auto">
-
         <div class="col-lg-5 d-flex justify-content-center align-items-center">
             <div class="bg-shadow">
                 <img src="/img/background/contact_desktop.jpg">
@@ -20,20 +19,17 @@ Tentang Kami
         </div>
         <div class="col-lg-6 offset-lg-1 d-flex justify-content-center align-items-center">
             <div class="position-relative">
-                <h4 class="content__counter">No. 01</h4>
-                <h2 class="content__title">Kami adalah salah satu Distributor Makanan Terbesar <span>di Bali...</span></h2>
-                {{-- <p class="content__body">Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio
-                    exercitationem delectus similique quam fuga praesentium minus, nemo veniam totam beatae et dolor
-                    sapiente alias. Iste corporis officiis ipsum aliquid odio!</p> --}}
+                <h4 class="counter">No. 01</h4>
+                <h2 class="section-title">Kami adalah salah satu Distributor Makanan Terbesar <span>di Bali...</span></h2>
             </div>
         </div>
     </div>
     <div class="content content-2 container row mx-auto">
         <div class="order-lg-1 order-2 col-lg-6">
             <div>
-                <h4 class="content__counter">No. 02</h4>
-                <h2 class="content__title">Visi</h2>
-                <div class="content__body mb-4">
+                <h4 class="counter">No. 02</h4>
+                <h2 class="section-title">Visi</h2>
+                <div class="content-2__body my-4">
                     <div class="media">
                         <img class="align-self-start mr-3" src="/img/icon/checkmark.svg">
                         <div class="media-body">
@@ -41,8 +37,8 @@ Tentang Kami
                         </div>
                     </div>
                 </div>
-                <h2 class="content__title">Misi</h2>
-                <div class="content__body">
+                <h2 class="section-title">Misi</h2>
+                <div class="content-2__body mt-4">
                     <div class="media">
                         <img class="align-self-start mr-3" src="/img/icon/checkmark.svg">
                         <div class="media-body">
@@ -81,8 +77,8 @@ Tentang Kami
         </div>
     </div>
     <div class="content content-3 container">
-        <h4 class="content__counter text-center">No. 03</h4>
-        <h2 class="content__title">Sejarah Perusahaan</h2>
+        <h4 class="counter mt-0 text-center">No. 03</h4>
+        <h2 class="section-title">Sejarah Perusahaan</h2>
         <div class="row mx-auto">
             <div class="col-12 history p-0">
                 <p>Usaha ini pertama kali dimulai ketika An Selamat membawa 1 mobil box Indomie dari Surabaya ke Bali pada tahun 1980. Dengan keuletan dan kegigihannya memperkenalkan Indomie di pasar Bali, Indomie menjadi dikenal dan diterima oleh masyarakat Bali. Tahun 2001 PT. Kembar Putra Makmur dibentuk untuk menjadi wadah dari usaha distribusi ini. Sejak saat itu, beberapa principal bergabung untuk menikmati jaringan distribusi yang sudah dikembangkan selama puluhan tahun. Sekarang, PT. Kembar Putra Makmur mendistribusikan lebih dari 100 jenis produk ke seluruh Pulau Bali.</p>
